3ede51af9a2776e0ba834e2495d435d0a2908f47
youssef aserrar <youssefasrar2966@gmail.com>
2024-04-14 15:13:55 +0100

    separate server from client

--- index.ts
+++ index.ts
@@ -4,16 +4,28 @@ const createEnv = <
   TS extends { [key: string]: ZodType<any> },
   TC extends { [key: string]: ZodType<any> }
 >(
-  schema: z.ZodObject<{
-    server: z.ZodObject<TS>;
-    client: z.ZodObject<TC>;
-  }>,
+  schema: {
+    server: { [key: string]: ZodType<any> };
+    client: { [key: string]: ZodType<any> };
+  },
   env: {
     server: { [K in keyof TS]: any };
     client: { [K in keyof TC]: any };
   }
 ) => {
-  const parsedEnv = schema.parse(env);
+  const _client = typeof schema.client === "object" ? schema.client : {};
+  const _server = typeof schema.server === "object" ? schema.server : {};
+  const client = z.object(_client);
+  const server = z.object(_server);
+  const isServer = typeof window === "undefined";
+
+  const allClient = client;
+  const allServer = server.merge(client);
+
+  const parsedEnv = isServer
+    ? allServer.parse({ ...env.server, ...env.client })
+    : allClient.parse({ ...env.client });
+
   return parsedEnv;
 };
 
